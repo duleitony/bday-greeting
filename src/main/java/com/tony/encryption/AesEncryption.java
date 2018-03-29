4f88f40e7858bcea18ef2c94ec657727bb3a2d66
Du, Lei | Tony | ESD <lei.du@rakuten.com>
2018-03-29 11:22:34 +0900

    finished encryption and decryption method with AES/CBC

--- src/main/java/com/tony/encryption/AesEncryption.java
+++ src/main/java/com/tony/encryption/AesEncryption.java
@@ -16,8 +16,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
-import org.apache.commons.codec.binary.Hex;
-
 /**
  * The class is used to try how to generate a AES key and how to encrypt and decrypt with a key and different mode
  * 
@@ -25,31 +23,51 @@ import org.apache.commons.codec.binary.Hex;
  *
  */
 public class AesEncryption {
+    final static String TRANSFORMATION = "AES/CBC/PKCS5Padding";
 
-    public static void main(String[] args) throws InvalidKeyException,
+    public static void main(String[] args)
+            throws InvalidKeyException,
     NoSuchAlgorithmException, NoSuchPaddingException, 
     InvalidParameterSpecException, IllegalBlockSizeException, 
-    BadPaddingException, InvalidAlgorithmParameterException, UnsupportedEncodingException {
+    BadPaddingException, InvalidAlgorithmParameterException, 
+    UnsupportedEncodingException 
+    {
+        String plainText = "Hello World!";
+        //By default, the key size is supported by Java8 is 128
         byte[] aes128Key = AesBinaryKeyGenerator(128);
-        String plainText = "Hello World";
-        String cipherText = encryptWithAesCbc(plainText, aes128Key);
-        System.out.println("cipher text of " + plainText + " is: " + cipherText);
+        IvParameterSpec ivParameterSpec = generateIv();
+        String cipherText = encryptWithAesCbc(plainText, aes128Key, ivParameterSpec);
+        String decryptedPlainText = dencryptWithAesCbc(cipherText, aes128Key, ivParameterSpec);
 
+        System.out.println("The cipher text of " + plainText + " is: " + cipherText);
+        System.out.println("The plain text of " + cipherText + " is: " + decryptedPlainText);
     }
 
-    public static String encryptWithAesCbc(String plaintext, byte[] key)
+    /**
+     * Encrypt function with AES CBC mode
+     * 
+     * @param plaintext
+     * @param key
+     * @param ivParameterSpec2 
+     * @return
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchPaddingException
+     * @throws InvalidParameterSpecException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     * @throws InvalidAlgorithmParameterException
+     * @throws UnsupportedEncodingException
+     */
+    public static String encryptWithAesCbc(String plaintext, byte[] key, IvParameterSpec ivParameterSpec)
             throws InvalidKeyException, NoSuchAlgorithmException,
             NoSuchPaddingException, InvalidParameterSpecException, 
             IllegalBlockSizeException, BadPaddingException, 
-            InvalidAlgorithmParameterException, UnsupportedEncodingException {
+            InvalidAlgorithmParameterException, UnsupportedEncodingException 
+    {
         byte[] cipherText;
         final SecretKey secretKey = new SecretKeySpec(key, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        //generate IV-
-        IvParameterSpec spec = 
-           cipher.getParameters().getParameterSpec(IvParameterSpec.class);
-        byte[] iv = spec.getIV();
-        final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
 
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
         cipherText = cipher.doFinal(plaintext.getBytes("UTF-8"));
@@ -57,8 +75,32 @@ public class AesEncryption {
         return DatatypeConverter.printHexBinary(cipherText);
     }
 
-    public static String dencryptWithAesCbc() {
-        return null;
+    /**
+     * Decrypt function with AES CBC mode
+     * 
+     * @param cipherText
+     * @param key
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchPaddingException
+     * @throws InvalidAlgorithmParameterException 
+     * @throws InvalidKeyException 
+     * @throws UnsupportedEncodingException 
+     * @throws BadPaddingException 
+     * @throws IllegalBlockSizeException 
+     */
+    public static String dencryptWithAesCbc(String cipherText, byte[] key, IvParameterSpec ivParameterSpec) 
+            throws NoSuchAlgorithmException, NoSuchPaddingException, 
+            InvalidKeyException, InvalidAlgorithmParameterException, 
+            IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException 
+    {
+        byte[] plainText;
+        final SecretKey secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+        plainText = cipher.doFinal(DatatypeConverter.parseHexBinary(cipherText));
+
+        return new String(plainText);
     }
 
     public static String encryptWithAesGcm() {
@@ -69,7 +111,34 @@ public class AesEncryption {
         return null;
     }
 
-    public static byte[] AesBinaryKeyGenerator(int keySize) {
+    /**
+     * The method is used to generate IV
+     * 
+     * @return
+     * @throws InvalidParameterSpecException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchPaddingException
+     */
+    public static IvParameterSpec generateIv() 
+            throws InvalidParameterSpecException, 
+            NoSuchAlgorithmException, NoSuchPaddingException 
+    {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        IvParameterSpec spec = 
+           cipher.getParameters().getParameterSpec(IvParameterSpec.class);
+        byte[] iv = spec.getIV();
+        final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        return ivParameterSpec;
+    }
+
+    /**
+     * AES key generator
+     * 
+     * @param keySize
+     * @return
+     */
+    public static byte[] AesBinaryKeyGenerator(int keySize) 
+    {
         final String ENCRYPTION_ALGORITHM_AES="AES";
         KeyGenerator keyGen;
         String hexKey = null;
@@ -84,7 +153,7 @@ public class AesEncryption {
             System.out.println("The length of binaryKey is : " + binaryKey.length + " bytes");
 
             hexKey = DatatypeConverter.printHexBinary(binaryKey);
-            System.out.println("AesKey in HEX is : " + hexKey);
+            System.out.println("The AesKey in HEX is : " + hexKey);
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
