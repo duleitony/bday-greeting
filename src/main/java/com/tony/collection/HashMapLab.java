d64a10fb9222bd8ab62385eca5f5cd0a87894493
lei.du <duleitony@gmail.com>
2018-06-20 09:37:24 +0900

    added 8583 code

---
+++ src/main/java/com/tony/collection/HashMapLab.java
@@ -0,0 +1,8 @@
+package com.tony.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapLab {
+    private Map<String, String> hashmap = new HashMap<String, String>();
+}
