991a5accd167e456ea463c3bb044cf7be81ac0cf
Lei Du <lei.du@gotouche.com>
2018-11-02 14:44:29 +0900

    stream and list

--- src/main/java/com/tony/java8/StreamTest.java
+++ src/main/java/com/tony/java8/StreamTest.java
@@ -1,7 +1,11 @@
 package com.tony.java8;
 
+import com.google.common.collect.Lists;
+
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamTest {
     public static void main(String[] args){
@@ -13,5 +17,30 @@ public class StreamTest {
                 .map(String::toUpperCase)
                 .sorted()
                 .forEach(System.out::println);
+
+
+        //从一列单词中选出以字母a开头的单词，按字母排序后返回前3个
+        List<String> list = Lists.newArrayList("are", "where", "anvato", "java", "abc");
+
+        //implement by stream
+        list.stream()
+                .filter(s -> s.startsWith("a"))
+                .sorted()
+                .limit(3)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        //implement without stream
+        List<String> tempList = Lists.newArrayList();
+        List<String> result = Lists.newArrayList();
+        for( int i = 0; i < list.size(); i++) {
+            if (list.get(i).startsWith("a")) {
+                tempList.add(list.get(i));
+            }
+        }
+        tempList.sort(Comparator.naturalOrder());
+        result = tempList.subList(0,3);
+        result.iterator();
+        result.forEach(System.out::println);
     }
 }
